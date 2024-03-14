4fc9f9e5a529f570bbee1bdbe7025fc7d7bf3a2d
alex_stivi <alexstividor@gmail.com>
2024-03-14 10:57:21 +0200

    add Material Design

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,35 +1,105 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    MessageCard("Android")
+            ComposeTutorialTheme {
+                Surface {
+                    MessageCard(Message("Android", "JetpackCompose"))
                 }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+
+        Column(modifier = Modifier.padding(5.dp)) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+            // Add a vertical space between the author and message texts
+            Spacer(
+                modifier = Modifier.height(20.dp)
+            )
+
+        }
 
     }
 }
 
-@Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
-}
+@Preview(name = "Light Mode")
 
-@Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message(
+                    "Jenny", "Shirkovets"
+                )
+            )
+        }
+    }
 }
